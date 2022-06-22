3e0481f6fb25ccf3120dc588fcd389c4e00175f9
ImbaMDT <metinpostfach@gmx.de>
2022-06-22 14:41:36 +0200

    try to include classes

--- json_writer.py
+++ json_writer.py
@@ -1,47 +1,51 @@
 import json
 from readProcessMemory import Read_MEM
 
-# writes and reads from an json file
+# writes and reads from a json file
 
-def read_json():
+# TODO: rework the iterations
+#
+
+class Read_Json:
+
+    """ passing variables from "readProcessMemory" to the Json file "game_infos" """
     with open('game_infos.json') as f:
         data = json.load(f)
 
-    return data
+    def write_json(self):
+        with open('game_infos.json', 'w') as f:
+            json.dump(self.data, f, indent=4, sort_keys=True)
 
-def write_json(data):
-    with open('game_infos.json', 'w') as f:
-        json.dump(data, f, indent=4, sort_keys=True)
+    """ passing the playernames """
+    def player_names(self):                # changes playernames
+        player_id = Read_MEM().player_pointers()
+        x = 0
+        y = 1
+        for i in range(8):
+            for item in self.data['players']['%s' %player_id[x]]:
+                item['playername'] = '%s' %player_id[y]
+                print(item['playername'])
+                Read_Json.write_json(self.data)
+            x += 2
+            y += 2
 
-def player_names():                # changes playernames
-    data = read_json()
-    player_id = Read_MEM.player_pointers()
-    x = 0
-    y = 1
-    for i in range(8):
-        for item in data['players']['%s' %player_id[x]]:
-            item['playername'] = '%s' %player_id[y]
-            print(item['playername'])
-            write_json(data)
-        x += 2
-        y += 2
-
-def champions():
-    data = read_json()
-    champ_id = Read_MEM.champ_pointers()
-    x = 0
-    y = 1
-    z = 2
-    for i in range (128):
-        for item in data['champs']['%s' %champ_id[x]]['%s' %champ_id[y]]:
-            item['champname'] = '%s' % champ_id[z]
-            print(item['playername'])
-        x += 3
-        y += 3
-        z += 3
+    """ passing the championnames and coords """
+    def champion_names(self):
+        champ_id = Read_MEM.champ_pointers()
+        x = 0
+        y = 1
+        z = 2
+        for i in range(128):                    # find a better way to iterate through an expending list
+            for item in self.data['champs']['%s' %champ_id[x]]['%s' %champ_id[y]]:
+                item['champname'] = '%s' % champ_id[z]
+                print(item['playername'])
+            x += 3
+            y += 3
+            z += 3
 
 def test():
-    data = read_json()
+    with open('game_infos.json') as f:
+        data = json.load(f)
     champ_id = '13284'
     champ_id2 = '26396'
     for item in data['champs']['%s' %champ_id]['%s' %champ_id2]:
@@ -52,8 +56,7 @@ def test():
 
 
 if __name__ == "__main__":
-
-    champions()
+    Read_Json().player_names()
 
 #    with open('set_updates.json', 'w') as f:
 #        json.dump(data, f, indent=4, sort_keys=True)
--- readProcessMemory.py
+++ readProcessMemory.py
@@ -8,16 +8,57 @@ import constants
 # TODO: get player coords
 #       add the players into the constants.Field dict ? or add it into the json file.
 #       get gold, shops, items, stage, exp left to level, get stage state (for example: can you drag and drop units?)
+#       check if you are able to read playernames
 
 class Read_MEM:
 
-    def __int__(self):
-        self.mem = Pymem(constants.PROCESS_NAME)
+    """ reads the game memory """
+    in_game = False
+    while not in_game:
+        try:
+            # get mem
+            mem = Pymem(constants.PROCESS_NAME)
+            mem.read_int(mem.base_address)
+            in_game = True
+        except:
+            pass
+    # other code that uses result but is not involved in getting it
+#    mem = Pymem(constants.PROCESS_NAME)
 
+    """ getting the playernames and playercoords """
+    def player_pointers(self):
+        # given constants.Process_Name
+        # returns Name and Pos of the players
+        player_offsets = []
+        player_offset = self.mem.read_int(self.mem.base_address + constants.oTFTChamps)
+        player_offset2 = self.mem.read_int(player_offset + 0x04)
+
+        for x in range(8):  # max loops
+            field_iter = self.mem.read_uint(player_offset2 + (0x04 * x))
+            try:
+                field_object_bytes = self.mem.read_bytes(field_iter + 0x58, 0x4)
+                if field_object_bytes == b'Obje':  # player_name is too large and a pointer now
+                    player_name_offset = self.mem.read_uint(field_iter + constants.oName)
+                    player_name = self.mem.read_string(player_name_offset)
+                    player_id = str(player_x + player_y)
+                    player_offsets.extend((player_id, player_name))
+                else:
+                    player_name = self.mem.read_string(field_iter + constants.oName)
+                    player_x = round(self.mem.read_float(field_iter + constants.oX) + self.mem.read_float(field_iter + constants.oX2))
+                    player_y = round(self.mem.read_float(field_iter + constants.oY) + self.mem.read_float(field_iter + constants.oY2))
+                    player_id = str(player_x + player_y)
+                    player_offsets.extend((player_id, player_name))
+
+            except MemoryReadError:
+                break
+
+        return player_offsets
+
+    """ getting every champ existing atm """
     def champ_pointers(self):
-        # Given constants.Process_Name
+        # given constants.Process_Name
         # returns Object Name and Pos
-        field_offset = self.mem.read_int(mem.base_address + constants.oTFTObject)
+        field_offset = self.mem.read_int(self.mem.base_address + constants.oTFTObject)
         field_entity_object = self.mem.read_uint(field_offset + 0x04)
 
         for x in range(256):  # max loops
@@ -48,36 +89,7 @@ class Read_MEM:
         return champ_x, champ_y, champ_name[0]
 
 
-    def player_pointers(self):
-        # Given constants.Process_Name
-        # returns Name and Pos of the players
-        player_offsets = []
-        player_offset = self.mem.read_int(self.mem.base_address + constants.oTFTChamps)
-        player_offset2 = self.mem.read_int(player_offset + 0x04)
-
-        for x in range(9):  # max loops
-            field_iter = self.mem.read_uint(player_offset2 + (0x04 * x))
-            try:
-                field_object_bytes = self.mem.read_bytes(field_iter + 0x58, 0x4)
-                if field_object_bytes == b'Obje':  # player_name is too large and a pointer now
-                    player_name_offset = self.mem.read_uint(field_iter + constants.oName)
-                    player_name = self.mem.read_string(player_name_offset)
-                    player_id = str(player_x + player_y)
-                    player_offsets.extend((player_id, player_name))
-                else:
-                    player_name = self.mem.read_string(field_iter + constants.oName)
-                    player_x = round(self.mem.read_float(field_iter + constants.oX) + self.mem.read_float(field_iter + constants.oX2))
-                    player_y = round(self.mem.read_float(field_iter + constants.oY) + selfmem.read_float(field_iter + constants.oY2))
-                    player_id = str(player_x + player_y)
-                    player_offsets.extend((player_id, player_name))
-
-            except MemoryReadError:
-                break
-
-        return player_offsets
-
-
 if __name__ == "__main__":
-    mem = Pymem(constants.PROCESS_NAME)
 
-    Read_MEM.player_pointers()
+    p = Read_MEM()
+    p.player_pointers()
